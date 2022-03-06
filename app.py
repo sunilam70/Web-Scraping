0ba9eb4e0af4b9a926e76a969fc182d15210b255
sunilam70 <96486057+sunilam70@users.noreply.github.com>
2022-03-06 18:14:09 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def scrape():
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 
-.update_one(query_parameter, {"$set": data}, options)
+#.update_one(query_parameter, {"$set": data}, options)
 
 if __name__ == "__main__":
-   app.run()
+   app.run(debug=True)
